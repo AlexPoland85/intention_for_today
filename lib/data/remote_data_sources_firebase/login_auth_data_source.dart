c2ad378f4f45d781e293847b97744421ab810535
Alex Pawlowski <xander.pawlowski@gmail.com>
2024-02-14 11:47:53 +0100

    Create items and login dataSource

---
+++ lib/data/remote_data_sources_firebase/login_auth_data_source.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginAuthDataSource {
+  Future<void> signIn(String email, String password) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (error) {
+      throw Exception(error.message);
+    }
+  }
+
+  Future<void> createUser(String email, String password) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (error) {
+      throw Exception(error.message);
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } on FirebaseAuthException catch (error) {
+      throw Exception(error.message);
+    }
+  }
+
+  Stream<User?> start() {
+    return FirebaseAuth.instance.authStateChanges();
+  }
+}
