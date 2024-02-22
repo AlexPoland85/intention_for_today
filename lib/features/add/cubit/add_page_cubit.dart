7c4243845f3e8d3d35a6786542d6db6c642ef39f
Alex Pawlowski <xander.pawlowski@gmail.com>
2024-02-22 15:04:37 +0100

    addpage

---
+++ lib/features/add/cubit/add_page_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:intention_for_today/app/core/enums.dart';
+import 'package:intention_for_today/domain/repositories/items_repository.dart';
+import 'dart:async';
+
+part 'add_page_cubit.freezed.dart';
+part 'add_page_state.dart';
+
+class AddPageCubit extends Cubit<AddPageState> {
+  AddPageCubit(this._itemsRepository) : super(AddPageState());
+
+  final ItemsRepository _itemsRepository;
+
+  Future<void> addUsersItem({required String content}) async {
+    emit(
+      AddPageState(
+        status: Status.loading,
+      ),
+    );
+    try {
+      await _itemsRepository.addUsersItem(content);
+      emit(
+        AddPageState(status: Status.success),
+      );
+    } catch (error) {
+      emit(
+        AddPageState(
+          status: Status.error,
+          errorMessage: error.toString(),
+        ),
+      );
+    }
+  }
+}
